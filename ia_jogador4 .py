581b2aeae0b2c07fccd18ffa83f5e9472484be6e
Carlos Antonio <carlosantonioob@gmail.com>
2026-04-26 18:17:08 -0300

    foi inseriro a ia do gambeta e trump no main

---
+++ ia_jogador4 .py
@@ -0,0 +1,4 @@
+from ia_base_defensiva import criar_decisor
+
+
+decidir_acao = criar_decisor(4, "q_table_jogador4.json", "log_jogador4.txt")
--- ia_jogador4.py
+++
@@ -1,6 +0,0 @@
-import random
-
-ACOES = ["cima", "baixo", "esquerda", "direita", "bomba", "parado"]
-
-def decidir_acao(player, mapa, jogadores, bombas, tempo_restante, pontos, hud_info, self_state):
-    return random.choice(ACOES)
